a9b7aafd9682f8418bbc5d903baaec538b935a11
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-10-18 13:15:14 +0100

    remove move counts
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-mod cli;
-
-use clap::{arg, Parser};
-use cli::Args;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
+
+use clap::Parser;
+use regex::Regex;
+
+use cli::Args;
+
+mod cli;
 
 // The output is wrapped in a Result to allow matching on errors.
 // Returns an Iterator to the Reader of the lines of the file.
@@ -15,6 +18,14 @@ where
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn remove_move_counts(line: &str) -> String {
+    let haystack = String::from(line);
+    let pattern = Regex::new(r"\d+\.\s+").unwrap();
+
+    let result = pattern.replace_all(&haystack, "");
+    result.to_string()
 }
 
 fn main() {
@@ -44,5 +55,23 @@ fn main() {
             }
         }
         println!("Found {count} games")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::remove_move_counts;
+
+    #[test]
+    fn test_remove_move_counts() {
+        // Arrange
+        let input = "1. Nf3 d5 2. c4 d4 3. d3 Nc6 4. g3 e5 5. Bg2 Bb4+ 6. Bd2 a5 7. O-O Nf6 8. Na3";
+        let expected_output = "Nf3 d5 c4 d4 d3 Nc6 g3 e5 Bg2 Bb4+ Bd2 a5 O-O Nf6 Na3";
+
+        // Act
+        let actual_output = remove_move_counts(input);
+
+        // Assert
+        assert_eq!(expected_output, actual_output)
     }
 }
