9f50d56acdb5c454a55dd9ca636f335187d1400e
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-10-18 11:31:40 +0100

    test arg parsing
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,8 @@ use clap::Parser;
 use cli::Args;
 
 fn main() {
-    let args = Args::parse();
+    let args: Args = Args::parse();
+    let n_threads = args.n_threads;
+    let batch_size = args.batch_size;
+    println!("n threads: {}; batch size: {}", n_threads, batch_size)
 }
