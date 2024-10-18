d36b8879742d7d5478cdd2ee22a57d7c6193378f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-10-18 11:32:36 +0100

    Cli (#1)
    * install clap
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * cli struct
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * parse args
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * default to single thread
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * test arg parsing
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    ---------
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod cli;
+
+use clap::Parser;
+use cli::Args;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+    let n_threads = args.n_threads;
+    let batch_size = args.batch_size;
+    println!("n threads: {}; batch size: {}", n_threads, batch_size)
 }
